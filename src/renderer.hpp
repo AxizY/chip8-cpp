cf70648724af95fb1f6ec2543fb202c44a5b3136
AxizY <24615015+AxizY@users.noreply.github.com>
2022-10-23 19:31:46 -0400

    Update renderer.hpp

--- src/renderer.hpp
+++ src/renderer.hpp
@@ -1,9 +1,12 @@
 #ifndef RENDERER_HPP
 #define RENDERER_HPP
-#include <GLFW/glfw3.h>
-#include <iostream>
+#include "../include/imgui/imgui.h"
+#include "../include/imgui/imgui_impl_glfw.h"
+#include "../include/imgui/imgui_impl_opengl3.h"
 #include "engine.hpp"
 #include "shader.hpp"
+#include <GLFW/glfw3.h>
+#include <iostream>
 #include <bitset>
 #include <cmath>
 
@@ -49,19 +52,19 @@ void keyf(GLFWwindow* window, int key, int scancode, int action, int mods) {
 
 template <size_t width, size_t height>
 class Screen {
-    private:
-        std::bitset<width * height> pixels;
     public:
+        uint8_t pixels[width * height];
+
         Screen() = default;
         void changePixel(size_t w, size_t h, bool v) {
             if(w <= 63 && h <= 31) {
-                pixels.set(w+width*h, v);
+                pixels[w+h*width] = v*255;
             }
         }
 
         int getPixel(size_t w, size_t h) {
             if(w <= 63 && h <= 31) {
-                return pixels[w+width*h];
+                return pixels[w+h*width];
             }
             return 0;
         }
@@ -73,7 +76,6 @@ class Screen {
                 {
                     changePixel(x, y, 0);
                 }
-                
             }
         }
 };
@@ -81,24 +83,50 @@ class Screen {
 class Display {
     private:
         GLFWwindow* window;
-        Screen<64, 32>* screen;
+        Screen<64, 32> screen;
         Drawer drawer;
         Shader shader;
         VertexArray* va;
         unsigned int texture;
         double lastCycle = 0;
         double lastTimer = 0;
-    public:
-        bool shouldClose() {
-            return glfwWindowShouldClose(window);
+        bool draw = false;
+
+        // imgui setup
+        void ImGUIinit() {
+            IMGUI_CHECKVERSION();
+            ImGui::CreateContext();
+            ImGuiIO& io = ImGui::GetIO(); (void)io;
+            ImGui::StyleColorsDark();
+            ImGui_ImplGlfw_InitForOpenGL(window, true);
+            ImGui_ImplOpenGL3_Init("#version 330");
         }
+
+        void ImGUInewFrame() {
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+        }
+
+        void ImGUIrender() {
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        }
+
+        void ImGUIFPS() {
+            ImGui::Begin("test");
+            ImGui::Text("hi");
+            ImGui::End();
+        }
+        // imgui setup end
 
         void createPlane() {
             float vertices[] = {
-                 0.0f,  0.0f, 0.0f,
-                20.0f,  0.0f, 0.0f,
-                20.0f, 20.0f, 0.0f,
-                 0.0f, 20.0f, 0.0f
+              // positions         // tex coords
+                -1.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+                1.0f,  -1.0f, 0.0f, 1.0f, 1.0f,
+                1.0f,   1.0f, 0.0f, 1.0f, 0.0f,
+                -1.0f,  1.0f, 0.0f, 0.0f, 0.0f,
             };
             unsigned int indices[] = {
                 0, 1, 2,
@@ -106,10 +134,25 @@ class Display {
             };
 
             va = new VertexArray(vertices, sizeof(vertices), indices, sizeof(indices));
+
+            glGenTextures(1, &texture);
+            glBindTexture(GL_TEXTURE_2D, texture); 
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // need this for it to work
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); // need this for it to work bruh
+
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 64, 32, 0, GL_RED, GL_UNSIGNED_BYTE, screen.pixels);
+
         }
 
-        Display(Screen<64, 32>* pScreen) {
-            screen = pScreen;
+    public:
+        bool shouldClose() {
+            return glfwWindowShouldClose(window);
+        }
+
+        Display() {
+            screen.clear();
+
             glfwInit();
 
             glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -131,45 +174,24 @@ class Display {
             shader.program();
 
             createPlane();
-        }
-    
-        void pollEvents() {
-            glfwPollEvents();
+
+            ImGUIinit();
         }
 
-        void loop() {
-            glClearColor(0.11f, 0.11f, 0.11f, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            projectionMatrix(&shader, window);
-            for(int h = 0; h < 32; h++) {
-                for (int w = 0; w < 64; w++) {
-                    if (screen->getPixel(w, h) == 1) {
-                        shader.setVec2("position", (float)w, (float)h);
-                        drawer.draw(va, &shader, 6);
-                    }
-                }
-            }
-
-            glfwSwapBuffers(window);
-        }
-
-        int end() {
-            glfwTerminate();
-            glfwPollEvents();
-            return 0;
+        uint8_t getKey(size_t i) {
+            return keys[i];
         }
 
         void clear() {
-            screen->clear();
+            screen.clear();
         }
 
         void changePixel(size_t w, size_t h, bool v) {
-            screen->changePixel(w, h, v);
+            screen.changePixel(w, h, v);
         }
 
         int getPixel(size_t w, size_t h) {
-            return screen->getPixel(w, h);
+            return screen.getPixel(w, h);
         }
 
         bool checkCycle() {
@@ -180,7 +202,7 @@ class Display {
             return false;
         }
 
-        bool checkTimer() {
+        bool checkTimers() {
             if(glfwGetTime() - lastTimer > (1.0f/60.0f)) {
                 lastTimer = glfwGetTime();
                 return true;
@@ -188,8 +210,38 @@ class Display {
             return false;
         }
 
-        uint8_t getKey(size_t i) {
-            return keys[i];
+        void preCycle() {
+            ImGUInewFrame();
+        }
+
+        void loop() {
+            glClearColor(0.11f, 0.11f, 0.11f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            drawer.draw(va, &shader, 6);
+
+            draw = true;
+        }
+
+        void updateTexture() {
+            glBindTexture(GL_TEXTURE_2D, texture); 
+            glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 64, 32, GL_RED, GL_UNSIGNED_BYTE, screen.pixels);
+        }
+
+        void postCycle() {
+            ImGUIFPS();
+            ImGUIrender();
+            if(draw) {
+                glfwSwapBuffers(window);
+                draw = false;
+            }
+            glfwPollEvents();
+        }
+
+        int end() {
+            glfwTerminate();
+            glfwPollEvents();
+            return 0;
         }
 };
 #endif
